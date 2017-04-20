84b72535d9e371d873709d770fac56dd0d2b31c1
bcorrel2 <bcorrel2@illinois.edu>
2017-04-20 16:36:57 -0500

    Interrupts

--- spimbot.s
+++ spimbot.s
@@ -136,3 +136,74 @@ main:
 	
     j   main
 
+
+.kdata				# interrupt handler data (separated just for readability)
+chunkIH:	.space 8	# space for two registers
+non_intrpt_str:	.asciiz "Non-interrupt exception\n"
+unhandled_str:	.asciiz "Unhandled interrupt type\n"
+
+
+.ktext 0x80000180
+interrupt_handler:
+.set noat
+	move	$k1, $at		# Save $at                               
+.set at
+	la	$k0, chunkIH
+	sw	$a0, 0($k0)		# Get some free registers                  
+	sw	$a1, 4($k0)		# by storing them to a global variable     
+
+	mfc0	$k0, $13		# Get Cause register                       
+	srl	$a0, $k0, 2                
+	and	$a0, $a0, 0xf		# ExcCode field                            
+	bne	$a0, 0, non_intrpt         
+
+interrupt_dispatch:			# Interrupt:                             
+	mfc0	$k0, $13		# Get Cause register, again                 
+	beq	$k0, 0, done		# handled all outstanding interrupts     
+
+	and	$a0, $k0, BONK_MASK	 # is there a bonk interrupt?                
+	bne	$a0, 0, bonk_interrupt   
+
+	and	$a0, $k0, TIMER_MASK # is there a timer interrupt?
+	bne	$a0, 0, timer_interrupt
+
+	# add dispatch for other interrupt types here.
+	and     $a0, $k0, PLAYPEN_UNLOCK_INT_MASK
+	bne     $a0, 0, unlock_interrupt
+	
+	li	$v0, PRINT_STRING	# Unhandled interrupt types
+	la	$a0, unhandled_str
+	syscall 
+	j	done
+
+unlock_interrupt:
+
+      sw      $a1, PLAYPEN_UNLOCK_ACK  # acknowledge interrupt
+              # load playpen as target
+	      move $t1, $s0
+	      move $t3, $s1
+
+      j       interrupt_dispatch       # see if other interrupts are waiting
+
+bonk_interrupt:
+      sw      $a1, 0xffff0060($zero)   # acknowledge interrupt
+
+      li      $a1, 10                  #  ??
+      lw      $a0, 0xffff001c($zero)   # what
+      and     $a0, $a0, 1              # does
+      bne     $a0, $zero, bonk_skip    # this 
+      li      $a1, -10                 # code
+bonk_skip:                             #  do 
+      sw      $a1, 0xffff0010($zero)   #  ??  
+
+      j       interrupt_dispatch       # see if other interrupts are waiting
+      
+timer_interrupt:
+	sw	$a1, TIMER_ACK		# acknowledge interrupt
+
+	li	$t0, 90			# ???
+	sw	$t0, ANGLE		# ???
+	sw	$zero, ANGLE_CONTROL	# ???
+
+	lw	$v0, TIMER		# current time
+	add	$v0, $v0, 50000  
